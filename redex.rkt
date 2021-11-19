0d5dec7852dcbd03ab3acb68ff22452370fb5501
michaelrauh <michaelrauh@ymail.com>
2021-11-18 19:26:51 -0500

    first pass at mod-3 arithmetic. Very suboptimal.

--- redex.rkt
+++ redex.rkt
@@ -13,8 +13,20 @@
 (define addition-red
   (reduction-relation
    addition
-   (--> (in-hole C (+ 0 N))
+   (--> (in-hole C (+ N 0))
         (in-hole C N)
-        n-0)))
+        n-0n)
+   (--> (in-hole C (+ 1 1))
+        (in-hole C 2)
+        n-112)
+   (--> (in-hole C (+ 1 2))
+        (in-hole C 0)
+        n-120)
+   (--> (in-hole C (+ 2 2))
+        (in-hole C 1)
+        n-221)
+   (--> (in-hole C (+ N_1 N_2))
+        (in-hole C (+ N_2 N_1))
+        n-rev)))
 
-(traces addition-red (term (+ 0 2)))
+(traces addition-red (term (+ (+ (+ (+ 1 0) (+ 0 1)) (+ 2 1)) (+ 1 2))))
